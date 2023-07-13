3c5068cc9b0573583a808f2987a95aa7364a20a3
Christine <christine@zipcodersMBP13.localdomain>
2023-07-13 08:38:51 -0400

    pet and pet test are both completed - for now

--- src/main/java/io/zipcoder/polymorphism/Pet.java
+++ src/main/java/io/zipcoder/polymorphism/Pet.java
@@ -1,2 +1,24 @@
-package io.zipcoder.polymorphism;public class Pet {
+package io.zipcoder.polymorphism;
+
+public abstract class Pet {
+    // my class variables
+    private String name;
+
+    // Constructor
+    public Pet(){
+        this("");
+    }
+    public Pet(String name){
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract String speak();
 }
--- src/test/java/io/zipcoder/polymorphism/PetTest.java
+++ src/test/java/io/zipcoder/polymorphism/PetTest.java
@@ -1,4 +1,35 @@
+package io.zipcoder.polymorphism;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class PetTest {
-  
+    @Test
+    public void catInheritanceTest(){
+        // Given
+        // When
+        Pet p = new Cat();
+        // Then
+        Assert.assertTrue(p instanceof Pet);
+    }
+
+    @Test
+    public void dogInheritanceTest(){
+        // Given
+        // When
+        Pet p = new Dog();
+        // Then
+        Assert.assertTrue(p instanceof Pet);
+    }
+
+    @Test
+    public void axolotlInheritanceTest(){
+        // Given
+        // When
+        Pet p = new Axolotl();
+        // Then
+        Assert.assertTrue(p instanceof Pet);
+    }
 }
